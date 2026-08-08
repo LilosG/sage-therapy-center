5d8cc9a568e0de33e4bb96678588448a84b74c08
LilosG <mike@lilosgrowth.com>
2026-08-07 17:36:00 -0700

    feat: centralize inquiry form options

---
+++ src/data/inquiry.ts
@@ -0,0 +1,19 @@
+export const inquiryTherapyOptions = [
+  'Individual Therapy',
+  'Couples Counseling',
+  'Family Therapy',
+  'Teen Counseling',
+  'Premarital & Marriage Counseling',
+  'EMDR Therapy',
+  'Anxiety Therapy',
+  'Trauma & PTSD Therapy',
+  'Not sure yet',
+] as const;
+
+export const inquirySessionOptions = [
+  'In-person in Carlsbad',
+  'Telehealth',
+  'Either works for me',
+] as const;
+
+export const inquiryContactOptions = ['Email', 'Phone call', 'Text message'] as const;
