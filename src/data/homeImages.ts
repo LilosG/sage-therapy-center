44133387aa4e3c030fb2c3a74162a1d98672c5fc
LilosG <mike@lilosgrowth.com>
2026-08-10 12:22:49 -0700

    remove rejected homepage image from registry

--- src/data/homeImages.ts
+++ src/data/homeImages.ts
@@ -1,7 +1,5 @@
 import heroCoastalPath from '../assets/images/home/hero-coastal-path.webp';
-import botanicalShadow from '../assets/images/home/botanical-shadow.webp';
 
 export const homeImages = {
   hero: heroCoastalPath,
-  philosophy: botanicalShadow,
 } as const;
